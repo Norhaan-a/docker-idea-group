e68e841da2dc5f01716fa462e1fe8aa7fb06c7f2
Norhaan-a <norhaan.ahmad1@gmail.com>
2022-06-01 14:52:12 +0200

    changed and added things to the world-map branch

--- world-map/backend/index.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const { get } = require('express/lib/response');
-const path = require('path');
-const app = express();
-const port = process.env.PORT || 3000;
-app.use(express.static(path.join(__dirname, '../dist')));
-app.listen(port, () => console.log('Listening on http://localhost:' + port));
